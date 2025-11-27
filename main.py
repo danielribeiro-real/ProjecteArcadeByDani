36d63fd15c096d2118bc744d3fcfbc6f9343ced6
danielribeiro <danielribeiro@iesmontsia.org>
2025-11-27 15:50:51 +0100

    projecte

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import jocs
+
 while True:
     print("================================")    
     print("--BENVINGUT/DA AL MINI ARCADE--")
@@ -5,5 +7,15 @@ while True:
     print("1. Jugar Pedra Paper Tisores")
     print("2. Jugar a endevinar els numero")
     print("S per sortir")
-    print("Selecciona una opcio:")
-    opcio = input()
+    opcio = input("Tria una opcio: ")
+    if opcio == "1":
+        import jocs.Pedra_Paper_Tisores
+        jocs.Pedra_Paper_Tisor
+    elif opcio == "2":
+        import endevinar_numero
+        endevinar_numero.jugar()
+    elif opcio.upper() == "S":
+        print("Gracies per jugar! Fins aviat!")
+        break
+    else:
+        print("Opcio no valida, si us plau tria una opcio correcta.")
