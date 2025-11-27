c6426d82fad607405cce07bddb51ba4aed95c9d3
danielribeiro <danielribeiro@iesmontsia.org>
2025-11-27 15:33:56 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+while True:
+    print("================================")    
+    print("--BENVINGUT/DA AL MINI ARCADE--")
+    print("================================")
+    print("1. Jugar Pedra Paper Tisores")
+    print("2. Jugar a endevinar els numero")
+    print("S per sortir")
+    print("Selecciona una opcio:")
+    opcio = input()
